bd6ce31b01d48b1a2ef11a69ef3332b32dcba728
devendra solanki <solankidevendrasingh25@gmail.com>
2025-10-19 22:39:24 +0530

    9 LeetCode

---
+++ 9_LeetCode.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        return s == rev;
+    }
+};
