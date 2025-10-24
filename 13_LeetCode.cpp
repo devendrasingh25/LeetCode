b79186b3ac17e774bd95463faabfbb1c2b5d5750
devendra solanki <solankidevendrasingh25@gmail.com>
2025-10-24 22:07:00 +0530

    13 Leetcode

---
+++ 13_LeetCode.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> value = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        
+        int total = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+          
+            if (i + 1 < n && value[s[i]] < value[s[i + 1]]) {
+                total -= value[s[i]];
+            } else {
+                total += value[s[i]];
+            }
+        }
+
+        return total;
+    }
+};
