16ea219d6b99e5339a62150f06a2ddf7b76608d8
devendra solanki <solankidevendrasingh25@gmail.com>
2025-11-16 16:41:59 +0530

    476  LeetCode

---
+++ 476_LeetCode.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+using namespace std;
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = 0;
+        int temp = num;
+        
+        
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+        
+        return mask ^ num;  
+    }
+};
