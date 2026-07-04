a02037db0d5a70daf05f61fb7c13ce8752d2bd85
Devendra/ Solanki <solankidevendrasingh25@gmail.com>
2026-07-04 07:54:49 +0530

    Time: 59 ms (61.92%), Space: 120.5 MB (87.13%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,10 +1,11 @@
 class DisjointUnion {
 public:
-    vector<int> par, rank_;
+    vector<int> par, rank_, minEdge;
 
     DisjointUnion(int n) {
         par.resize(n);
         rank_.assign(n, 0);
+        minEdge.assign(n, INT_MAX);
         iota(par.begin(), par.end(), 0);
     }
 
@@ -13,49 +14,43 @@ public:
         return par[x] = find(par[x]);
     }
 
-    void unionbyrank(int a, int b) {
+    void unionbyrank(int a, int b, int w) {
         int parA = find(a);
         int parB = find(b);
 
-        if (parA == parB) return;
+        if (parA == parB) {
+            minEdge[parA] = min(minEdge[parA], w);
+            return;
+        }
 
         if (rank_[parA] == rank_[parB]) {
             par[parB] = parA;
+            minEdge[parA] = min({minEdge[parA], minEdge[parB], w});
             rank_[parA]++;
         }
         else if (rank_[parA] > rank_[parB]) {
             par[parB] = parA;
+            minEdge[parA] = min({minEdge[parA], minEdge[parB], w});
         }
         else {
             par[parA] = parB;
+            minEdge[parB] = min({minEdge[parA], minEdge[parB], w});
         }
     }
 };
 
 class Solution {
 public:
-   
     int minScore(int n, vector<vector<int>>& roads) {
-       DisjointUnion dsu(n+1);
+        DisjointUnion dsu(n + 1);
 
-       for( auto it : roads){
-         int u = it[0];
-         int v = it[1];
-          
-          dsu.unionbyrank( u, v) ;
-       }
+        for (auto it : roads) {
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            dsu.unionbyrank(u, v, w);
+        }
 
-       int t = dsu.find(1);
-       int mini = INT_MAX;
-        for( auto it : roads){
-         int u = it[0];
-         int v = it[1];
-          int cost = it[2];
-          
-          if( dsu.find(u) == t){
-            mini = min(mini,cost);
-          }
-       }
-       return mini;
+        return dsu.minEdge[dsu.find(1)];
     }
 };
