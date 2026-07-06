c0417fe1d98144838adfe0dd0e46467e19c21111
Devendra/ Solanki <solankidevendrasingh25@gmail.com>
2026-07-06 16:34:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.25%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,15 +4,20 @@ public:
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
 
-        vector<int> dp (n+1);
-        dp[0] = 0;
-        dp[1] = 1 ;
-        dp[2] = 1 ;
+      
+       int prv = 1;
+       int prv1 = 1 ;
+        int prv2 = 0 ;
+        
 
         for( int i = 3 ; i <= n ;i++){
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3] ;
+           int ans  = prv + prv1 + prv2 ;
+           prv2 = prv1;
+            prv1 = prv;
+            prv = ans;
+          
         }
 
-        return dp[n];
+        return prv ;
     }
 };
