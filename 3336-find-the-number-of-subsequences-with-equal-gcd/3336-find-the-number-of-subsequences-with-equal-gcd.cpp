03006d20cea6a3ad72b5ef859522c012e988dd4b
Devendra/ Solanki <solankidevendrasingh25@gmail.com>
2026-07-14 18:40:40 +0530

    Time: 1137 ms (46.95%), Space: 58.3 MB (65.22%) - LeetHub

---
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+     int MOD = 1e9+7 ;
+    int t[201][201][201] ;
+    int solve(vector<int>& nums, int i , int first ,int second){
+      
+       if( i == nums.size()){
+        bool bothempty = (first != 0 && second != 0 );
+        bool gcdmatch = (first  == second);
+
+        return (bothempty && gcdmatch) ? 1:0 ;
+       }
+       if( t[i][first][second] != -1 ) return t[i][first][second] ;
+      int skip =  solve( nums ,i+1 , first ,second);
+      int take1 = solve ( nums, i+1 , __gcd(nums[i] ,first) , second); 
+      int take2 = solve ( nums, i+1 , first ,__gcd( nums[i] , second)); 
+
+      return t[i][first][second] = (0LL+skip+take1+take2)%MOD ;
+    }
+    int subsequencePairCount(vector<int>& nums) {
+        memset(t,-1,sizeof(t));
+        return solve( nums, 0 , 0 ,0 );
+    }
+};
