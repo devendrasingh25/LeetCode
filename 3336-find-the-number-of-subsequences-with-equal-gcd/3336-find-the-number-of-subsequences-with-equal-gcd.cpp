9e142adbdbd90b5dbe2f784e37b47ac17fc1a39c
Devendra/ Solanki <solankidevendrasingh25@gmail.com>
2026-07-14 19:51:38 +0530

    Time: 394 ms (75.65%), Space: 292.2 MB (36.52%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-     int MOD = 1e9+7 ;
-    int t[201][201][201] ;
-    int solve(vector<int>& nums, int i , int first ,int second){
-      
-       if( i == nums.size()){
-        bool bothempty = (first != 0 && second != 0 );
-        bool gcdmatch = (first  == second);
-
-        return (bothempty && gcdmatch) ? 1:0 ;
-       }
-       if( t[i][first][second] != -1 ) return t[i][first][second] ;
-      int skip =  solve( nums ,i+1 , first ,second);
-      int take1 = solve ( nums, i+1 , __gcd(nums[i] ,first) , second); 
-      int take2 = solve ( nums, i+1 , first ,__gcd( nums[i] , second)); 
-
-      return t[i][first][second] = (0LL+skip+take1+take2)%MOD ;
-    }
+int MOD = 1e9+7;
     int subsequencePairCount(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
-        return solve( nums, 0 , 0 ,0 );
+        int m  = *max_element(nums.begin() , nums.end());
+        int n = nums.size();
+
+        vector<vector<int>> dp(m+1, vector<int>(m+1));
+        dp[0][0] = 1 ;
+        for( int i =0 ;i < n ;i++){
+            vector<vector<int>> ndp(m+1 , vector<int>(m+1));
+            for( int s1 =0 ; s1 <=m ; s1++){
+                for( int s2 = 0 ; s2 <= m ;s2++){
+                    int val = dp[s1][s2];
+                    if( val == 0 )continue ;
+
+                    int divisor1 = gcd(s1,nums[i]);
+                    int divisor2 = gcd (s2,nums[i]);
+
+                    ndp[s1][s2]= ( ndp[s1][s2] +val )% MOD ;
+                    ndp[divisor1][s2] =( ndp [divisor1][s2] +val)% MOD;
+                    ndp[s1][divisor2] = ( ndp[s1][divisor2] + val)% MOD;
+                }
+            }
+            dp.swap(ndp);
+        }
+        int ans = 0 ;
+        for( int i =1 ;i <= m ; i++ ){
+          ans = ( ans+ dp[i][i] )% MOD;
+        }
+        return ans;
     }
 };
