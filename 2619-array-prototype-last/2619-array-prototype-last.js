cece2914152a93d1041932cf94f0f40a4ab79159
Devendra/ Solanki <solankidevendrasingh25@gmail.com>
2026-07-15 14:59:37 +0530

    Time: 46 ms (33%), Space: 52.6 MB (93.84%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,7 +2,7 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    if (this.length === 0) {
+    if (!this.length ) {
         return -1;
     }
     return this[this.length - 1];
