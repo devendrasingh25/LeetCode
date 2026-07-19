5055bba11c96d365e6b7e3cae2b7a1384b74f476
Devendra/ Solanki <solankidevendrasingh25@gmail.com>
2026-07-19 14:15:06 +0530

    Time: 0 ms (100%), Space: 9.2 MB (84.28%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+       int n = s.size() ;
+       vector<int> lastindex(26);
+       vector<bool> taken( 26 ,false);  
+
+       string result ="";
+
+       for( int i = 0 ;i < n ;i++){
+         int idx = s[i] - 'a' ;
+
+         lastindex[idx] = i ;
+       }
+
+       for( int i =0 ;i < n ;i++){
+          int idx = s[i] - 'a' ;
+
+          if( taken[idx] == true) continue ;
+
+          while( result.length() > 0 &&  result.back() > s[i] && lastindex[result.back() - 'a'] > i ){
+            taken[result.back() - 'a'] = false ;
+            result.pop_back();
+          }
+
+          result.push_back(s[i]);
+          taken[idx] = true ;
+        
+       }
+
+       return result ;
+    }
+};
