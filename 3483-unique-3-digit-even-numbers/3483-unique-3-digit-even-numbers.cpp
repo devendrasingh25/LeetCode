a60960b47eb7e7269417a34439283ecfb90d1eac
Devendra Solanki <solankidevendrasingh25@gmail.com>
2026-09-12 22:56:07 +0530

    Time: 8 ms (56.27%), Space: 31 MB (51.58%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int n = digits.size();
+        set<int> results;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i) continue;
+                for (int l = 0; l < n; l++) {
+                    if (l == i || l == j) continue;
+
+                    if (digits[i] == 0) continue;       // no leading zero
+                    if (digits[l] % 2 != 0) continue;   // last digit must be even
+
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[l];
+                    results.insert(num);
+                }
+            }
+        }
+
+        return results.size();
+    }
+};
